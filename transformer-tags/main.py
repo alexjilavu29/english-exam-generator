5c872bd51a4492fc08ffff08225fec433b95adc1
lazar <dasidasi86@gmail.com>
2025-07-09 22:53:46 +0300

    replace strange ascii

--- transformer-tags/main.py
+++ transformer-tags/main.py
@@ -1,6 +1,18 @@
 import json
 
 
+def process_body(body):
+    new_body = body.replace("\u2026", ".")
+    new_body = new_body.replace("\u2019", "'")
+    new_body = new_body.replace("\u2018", "'")
+    new_body = new_body.replace("\u2013", "-")
+    new_body = new_body.replace('\u00A0', " ")
+    new_body = new_body.replace("\u201c", "'")
+    new_body = new_body.replace("\u201d", "'")
+    new_body = new_body.replace("\u00a3", "£")
+    return new_body
+
+
 def load_and_process_questions(json_path):
     with open(json_path, 'r', encoding='utf-8') as f:
         data = json.load(f)
@@ -13,7 +25,7 @@ def load_and_process_questions(json_path):
         correct_answer = item.get("correct_answer", 0)
 
         processed.append({
-            "body": body,
+            "body": process_body(body),
             "answers": answers,
             "tags": tags,
             "correct_answer": correct_answer
@@ -24,19 +36,39 @@ def load_and_process_questions(json_path):
 
 processed_questions = load_and_process_questions("questions.json")
 
-print(json.dumps(processed_questions, indent=2))
+print("Questions length:")
+print(len(processed_questions))
+
+# print(json.dumps(processed_questions, indent=2))
 
 questions_with_tags = [q for q in processed_questions if q["tags"]]
-questions_without_tags = [q for q in processed_questions if not q["tags"]]
+questions_without_tags = [
+    q for q in processed_questions if not q["tags"]]
 
-print("Questions with tags:\n")
-print(json.dumps(questions_with_tags, indent=2))
+# print("Questions with tags:\n")
+# print(json.dumps(questions_with_tags, indent=2))
 
-print("Questions without tags:\n")
-print(json.dumps(questions_without_tags, indent=2))
+# print("Questions without tags:\n")
+# print(json.dumps(questions_without_tags, indent=2))
 
 print("Questions with tags length:")
 print(len(questions_with_tags))
 
 print("Questions without tags length:")
 print(len(questions_without_tags))
+
+
+def contains_non_ascii(text):
+    return any(ord(char) > 127 and char != '£' for char in text)
+
+
+questions_with_funny_chars = [
+    q for q in processed_questions if contains_non_ascii(q["body"])
+
+
+]
+
+print("Questions with funny chars length:")
+print(len(questions_with_funny_chars))
+
+# print(json.dumps(questions_with_funny_chars, indent=2, ensure_ascii=False))
